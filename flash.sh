4805c775e0e386eaeb8e7f7d02aad189fc4945da
Henry Dang <phuongminh.dang@gmail.com>
2023-02-06 14:08:41 +0700

    Added flash script

---
+++ flash.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#******************************************************************#
+# Author     : Nishal
+# Date       : 5th June 2017
+# File       : build_setup.sh
+# Version    : 0.1
+# Description: This script does the following thing
+#              1)setup for the system to work when called with INITIAL
+#              2)git clone of opensource repo when called with UPDATE
+#              3)builds an application
+#
+#******************************************************************#
+
+#Export environment variable
+source setenv.sh
+
+if [ "$#" -ne  "0" -a "$1" == "INITIAL" ]; then
+    cd $DW_OT_ROOT/..
+    ./setup.sh $1
+elif [ "$#" -ne  "0" -a "$1" == "UPDATE" ]; then
+    cd $DW_OT_ROOT/..
+    ./setup.sh $1
+fi
+
+#Build command for application
+$OT_ROOT/scripts/build.sh
+
+echo "==========Flash the firmware========="
+
+echo "Device list"
+nrfjprog --ids
+
+echo "Flashing master device..."
+nrfjprog --snr 683103016 -f nrf52 --chiperase --program openthread-master/output/nrf52840/bin/ot-cli-ftd.hex
+
+echo "Flashing slave device..."
+nrfjprog --snr 683907792 -f nrf52 --chiperase --program openthread-master/output/nrf52840/bin/ot-cli-ftd.hex
+
+
